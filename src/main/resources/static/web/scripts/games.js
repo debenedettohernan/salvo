dfbfb2210ed84033ce3a0c1b00d66c12c7e66086
Hernan <hernan.debenedetto@hotmail.com>
2021-04-15 18:05:32 -0300

    end task 5 x2

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,3 +1,104 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        games: [],
+        scorePlayers: [],
+    },
+    methods: {
+
+        players: function() {
+            var players = [];
+            app.games.forEach(game => {
+                game.gamePlayers.forEach(gamePlayer => {
+
+                    if (!players.includes(gamePlayer.player.email)) {
+                        players.push(gamePlayer.player.email)
+                    }
+
+                })
+            });
+            return players;
+
+        },
+
+
+        totalScore: function(email) {
+            var total = 0
+            app.games.forEach(game =>
+                game.gamePlayers.forEach(gamePlayer => {
+                    if (email == gamePlayer.player.email) {
+                        total += gamePlayer.score
+                    }
+
+                }))
+            return total;
+
+        },
+
+        totalWins: function(email) {
+            var wins = 0
+            app.games.forEach(game =>
+                game.gamePlayers.forEach(gamePlayer => {
+
+                    if (email == gamePlayer.player.email) {
+                        wins += gamePlayer.score == 1
+                    }
+                }))
+            return wins;
+
+        },
+
+        totalTies: function(email) {
+            var ties = 0.0
+            app.games.forEach(game =>
+                game.gamePlayers.forEach(gamePlayer => {
+
+                    if (email == gamePlayer.player.email) {
+                        ties += gamePlayer.score == 0.5
+                    }
+                }))
+            return ties;
+        },
+
+        totalLosses: function(email) {
+            var losses = 0
+            app.games.forEach(game =>
+                game.gamePlayers.forEach(gamePlayer => {
+
+                    if (email == gamePlayer.player.email) {
+                        losses += gamePlayer.score == 0
+                    }
+                }))
+            return losses;
+        },
+
+
+        tablaScore: function() {
+            var scorePlayer = []
+            var totalPlayers = app.players()
+
+            totalPlayers.forEach(player => {
+                var total = app.totalScore(player)
+                var win = app.totalWins(player)
+                var ties = app.totalTies(player)
+                var losses = app.totalLosses(player)
+
+                scorePlayer = {
+                    email: player,
+                    totalScore: total,
+                    winScore: win,
+                    tiesScore: ties,
+                    lossesScore: losses
+
+                }
+
+                app.scorePlayers.push(scorePlayer)
+            })
+
+        }
+
+    }
+})
 fetch('http://localhost:8080/api/games')
     .then(function(respuesta) {
         return respuesta.json();
@@ -5,109 +106,6 @@ fetch('http://localhost:8080/api/games')
     .then(function(data) {
 
         app.games = data;
-        players();
-        totalScore();
-        totalWins();
-        totalTies()
-        totalLosses()
-        tablaScore();
+        app.players();
+        app.tablaScore();
     })
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        games: [],
-        scorePlayers: [],
-    }
-})
-
-function players() {
-    var players = [];
-    app.games.forEach(game => {
-        game.gamePlayers.forEach(gamePlayer => {
-
-            if (!players.includes(gamePlayer.player.email)) {
-                players.push(gamePlayer.player.email)
-            }
-
-        })
-    });
-    return players;
-
-};
-
-
-function totalScore(email) {
-    var total = 0
-    app.games.forEach(game =>
-        game.gamePlayers.forEach(gamePlayer => {
-            if (email == gamePlayer.player.email) {
-                total += gamePlayer.score
-            }
-
-        }))
-    return total;
-
-};
-
-function totalWins(email) {
-    var wins = 0
-    app.games.forEach(game =>
-        game.gamePlayers.forEach(gamePlayer => {
-
-            if (email == gamePlayer.player.email) {
-                wins += gamePlayer.score == 1
-            }
-        }))
-    return wins;
-
-};
-
-function totalTies(email) {
-    var ties = 0.0
-    app.games.forEach(game =>
-        game.gamePlayers.forEach(gamePlayer => {
-
-            if (email == gamePlayer.player.email) {
-                ties += gamePlayer.score == 0.5
-            }
-        }))
-    return ties;
-};
-
-function totalLosses(email) {
-    var losses = 0
-    app.games.forEach(game =>
-        game.gamePlayers.forEach(gamePlayer => {
-
-            if (email == gamePlayer.player.email) {
-                losses += gamePlayer.score == 0
-            }
-        }))
-    return losses;
-};
-
-
-function tablaScore() {
-    var scorePlayer = []
-    var totalPlayers = players()
-
-    totalPlayers.forEach(player => {
-        var total = totalScore(player)
-        var win = totalWins(player)
-        var ties = totalTies(player)
-        var losses = totalLosses(player)
-
-        scorePlayer = {
-            email: player,
-            totalScore: total,
-            winScore: win,
-            tiesScore: ties,
-            lossesScore: losses
-
-        }
-
-        app.scorePlayers.push(scorePlayer)
-    })
-
-};
